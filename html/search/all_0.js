85176a71746ab392d1f038eeea31aea189a56bec
Mestuq <48328332+Mestuq@users.noreply.github.com>
2022-03-15 13:16:48 +0100

    Dokumentacja Doxygen

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['bridgefunction_0',['BridgeFunction',['../class_ai_s_d_1_1_dynamic_array.html#a0bbfb35b3113cc7d5162323ac63baee6',1,'AiSD::DynamicArray']]]
+];
---
+++ html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "bdo",
+  1: "d",
+  2: "bo"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "related"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Friends"
+};
+

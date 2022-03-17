dca1ccabc7b0dbe1ddbd9a48271f4120aa33c363
Mestuq <48328332+Mestuq@users.noreply.github.com>
2022-03-17 23:28:31 +0100

    informacja na glownej stronie

--- include/DoxygenMainPage.h
+++ include/DoxygenMainPage.h
@@ -4,6 +4,7 @@
 *   <br>**Algorytmy Dynamiczna Tablica**<br>
 *   Damian Szopinski 185394<br>
 *   Maciej Pestka 170088<br>
+*   Szczegolowe informacje znajduja sie w pliku PDF! <br>
 *<a href="namespace_ai_s_d.html">Kliknij tutaj by przejsc do dokumentacji funkcji testujacych</a><br>
 *<a href="class_ai_s_d_1_1_dynamic_array.html">Kliknij tutaj by przejsc do dokumentacji Dynamic Array</a>
 */

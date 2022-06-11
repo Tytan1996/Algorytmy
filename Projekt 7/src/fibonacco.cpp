bc340b9fe106f21768d8e21b81bcc30ae3a947a9
Maciej Pestka <maciekpestka1990@gmail.com>
2022-06-11 14:46:20 +0200

    dodanie ciag fiboccaniego

---
+++ Projekt 7/src/fibonacco.cpp
@@ -0,0 +1,29 @@
+#include "fibonacco.h"
+
+#include <iostream>
+
+
+size_t CiagFic(int n){
+    if(n<2){
+        return n;
+    }
+    return CiagFic(n-2)+CiagFic(n-1);
+}
+
+size_t CiagFic1(int n){
+    if(n<2){
+         return n;
+    }
+    else{
+        size_t tmp, a=1,b=0;
+        for(size_t i=2;i<=n;++i){
+            tmp=a;
+            a+=b;
+            b=tmp;
+        }
+        return a;
+    }
+    return 0;
+}
+
+

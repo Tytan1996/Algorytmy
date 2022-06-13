40150749900c356ce739d21cb2e73fa7b2aa09ba
Maciej Pestka <maciekpestka1990@gmail.com>
2022-06-13 06:14:53 +0200

    praca na macierza

---
+++ Projekt 7/include/macierz.h
@@ -0,0 +1,10 @@
+#ifndef MACIERZ_H
+#define MACIERZ_H
+
+#include <vector>
+
+//void matrixChainOrder(int &tab);
+//int  MatrixChainMemoizedAux(int m[][],int p,int i,int n);
+
+
+#endif // MACIERZ_H
